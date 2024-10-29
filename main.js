84b5770ac8b96ed68b67caa5bff7fe730e0fbd3c
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-10-29 20:08:49 -0300

    feat: genetic algorithm

--- main.js
+++ main.js
@@ -11,26 +11,36 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 // Lane 0 1 2 3 ... (getLaneCenter)
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
 
-// Globals
-const N = 100;
+const N = 1;
 const cars = generateCars(N);
-let bestCar = cars[0];
-
-if (localStorage.getItem("bestBrain"))
-  bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
 
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
   new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
   new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
 ];
+
+const mutations = 0.3;
+let bestCar = cars[0];
+
+if (localStorage.getItem("bestBrain")) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+
+    if (i !== 0) NeuralNetwork.mutate(cars[i].brain, mutations);
+  }
+}
 
 animate();
 
 function generateCars(N) {
   const cars = [];
 
-  for (let i = 1; i < N; i++) {
+  for (let i = 0; i < N; i++) {
     cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
   }
 
